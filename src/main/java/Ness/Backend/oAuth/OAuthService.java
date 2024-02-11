d9e95004eb88a19dd1c06bc8185daf93f7a87fb5
JeonHaeseung <414catherine@gmail.com>
2024-02-11 16:04:57 +0900

    [feat] 구글 로그인 후 JWT 발급 과정 완료
    보일러플레이트 코드 다수 발생, 추후 리팩토링 필요

--- src/main/java/Ness/Backend/oAuth/OAuthService.java
+++ src/main/java/Ness/Backend/oAuth/OAuthService.java
@@ -1,34 +1,44 @@
 package Ness.Backend.oAuth;
 
+import Ness.Backend.auth.AuthDetails;
+import Ness.Backend.auth.jwt.JwtTokenProvider;
+import Ness.Backend.domain.Member;
+import Ness.Backend.member.MemberRepository;
 import Ness.Backend.oAuth.dto.GoogleResponseDto;
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.http.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class OAuthService {
     private final Environment env;
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public OAuthService(Environment env) {
-        this.env = env;
-    }
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
     public GoogleResponseDto socialLogin(String code, String registration) {
         String accessToken = getAccessToken(code, registration);
         JsonNode userResourceNode = getUserResource(accessToken, registration);
-        System.out.println("userResourceNode = " + userResourceNode);
-
         String id = userResourceNode.get("id").asText();
         String email = userResourceNode.get("email").asText();
         String picture = userResourceNode.get("picture").asText();
-
-        System.out.println("id = " + id);
-        System.out.println("email = " + email);
-        System.out.println("picture = " + picture);
+        String message = checkSignUp(email, id);
 
         GoogleResponseDto googleResponseDto = GoogleResponseDto.builder()
                 .picture(picture)
@@ -38,7 +48,40 @@ public class OAuthService {
         return googleResponseDto;
     }
 
+    public String checkSignUp(String email, String password){
+        //이런 코드로도 괜찮은 것인가...
+
+        /* 사용자가 제출한 이메일과 비밀번호 확인하기 */
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+
+        /* 사용자 인증 완료 */
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        /* 인증이 되었을 경우 */
+        if(authentication.isAuthenticated()) {
+            /* 사용자가 인증되면 AuthDetails 객체가 생성되어 Authentication 객체에 포함되고,
+             * 이 AuthDetails 객체를 통해서 인증된 사용자의 정보를 확인 가능 */
+            AuthDetails authDetails = (AuthDetails) authentication.getPrincipal();
+
+            Long authenticatedId  = authDetails.getMember().getId();
+            String authenticatedEmail = authDetails.getMember().getEmail();
+
+            /* JWT 토큰 반환 */
+            return jwtTokenProvider.generateJwtToken(authenticatedId, authenticatedEmail);
+        }
+        else{ /* 인증이 되지 않았을 경우 */
+            Member member = Member.builder()
+                    .email(email)
+                    .password(bCryptPasswordEncoder.encode(password)) //비밀번호는 해싱해서 DB에 저장
+                    .build();
+
+            memberRepository.save(member); // DB에 저장하기
+        }
+        return "회원가입/로그인 체크 완료";
+    }
+
     private String getAccessToken(String authorizationCode, String registration) {
+        //여기 그냥 @Value로 바꿀지 고민 중
         String clientId = env.getProperty("oauth2." + registration + ".client-id");
         String clientSecret = env.getProperty("oauth2." + registration + ".client-secret");
         String redirectUri = env.getProperty("oauth2." + registration + ".redirect-uri");
@@ -62,7 +105,7 @@ public class OAuthService {
     }
 
     private JsonNode getUserResource(String accessToken, String registration) {
-        String resourceUri = env.getProperty("oauth2."+registration+".resource-uri");
+        String resourceUri = env.getProperty("oauth2." + registration + ".resource-uri");
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + accessToken);

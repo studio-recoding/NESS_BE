f6d2c72f76d6b95ebe215d95859668d1185cc41b
JeonHaeseung <414catherine@gmail.com>
2024-03-11 12:57:36 +0900

    [feat] 구글 로그인관련 OpenFeign 설정 및 DTO 추가

--- src/main/java/Ness/Backend/domain/auth/oAuth/OAuth2Service.java
+++ src/main/java/Ness/Backend/domain/auth/oAuth/OAuth2Service.java
@@ -1,24 +1,24 @@
 package Ness.Backend.domain.auth.oAuth;
 
+import Ness.Backend.domain.auth.oAuth.dto.GoogleResourceDto;
+import Ness.Backend.domain.auth.oAuth.dto.GoogleTokenDto;
 import Ness.Backend.domain.auth.security.AuthDetails;
 import Ness.Backend.domain.auth.jwt.JwtTokenProvider;
 import Ness.Backend.domain.profile.ProfileRepository;
 import Ness.Backend.domain.profile.entity.Profile;
 import Ness.Backend.domain.member.entity.Member;
 import Ness.Backend.domain.member.MemberRepository;
-import com.fasterxml.jackson.databind.JsonNode;
+import Ness.Backend.global.auth.oAuth.dto.GoogleOAuthApi;
+import Ness.Backend.global.auth.oAuth.dto.GoogleResourceApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.util.Pair;
-import org.springframework.http.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -28,13 +28,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Transactional
 public class OAuth2Service {
     private final Environment env;
-    private final RestTemplate restTemplate = new RestTemplate();
 
     private final MemberRepository memberRepository;
     private final ProfileRepository profileRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final GoogleOAuthApi googleOAuthApi;
+    private final GoogleResourceApi googleResourceApi;
 
     public String socialLogin(String code, String registration) {
         /*
@@ -45,10 +46,10 @@ public class OAuth2Service {
         * 4. 존재하는 유저가 맞을 경우 jwt 토큰 반환, 아니라면 email과 id를 각각 아이디와 패스워드로 DB에 저장
          * */
         String accessToken = getAccessToken(code, registration);
-        JsonNode userResourceNode = getUserResource(accessToken, registration);
-        String id = userResourceNode.get("id").asText();
-        String email = userResourceNode.get("email").asText();
-        String picture = userResourceNode.get("picture").asText();
+        GoogleResourceDto googleResourceDto = getUserResource(accessToken, registration);
+        String id = googleResourceDto.getId();
+        String email = googleResourceDto.getEmail();
+        String picture = googleResourceDto.getPicture();
 
         Pair<Boolean, String> result = checkSignUp(email, id);
 
@@ -77,8 +78,7 @@ public class OAuth2Service {
             return "회원가입이 완료되었습니다.";
 
         } catch (DataIntegrityViolationException e) {
-            /* 중복된 이메일 값이 삽입되려고 할 때 발생하는 예외 처리 */
-            //에러-여기서 Unique 값이 적용되지 않음
+            /* 중복된 이메일 값이 삽입되려고 할 때 발생하는 예외 처리,unique = true 때문에 발생하는 에러 */
             return "이미 사용 중인 이메일 주소입니다.";
         }
     }
@@ -96,49 +96,35 @@ public class OAuth2Service {
              * 이 AuthDetails 객체를 통해서 인증된 사용자의 정보를 확인 가능 */
             AuthDetails authDetails = (AuthDetails) authentication.getPrincipal();
 
-            Long authenticatedId  = authDetails.getMember().getId();
-            String authenticatedEmail = authDetails.getMember().getEmail();
+            String authEmail  = authDetails.getMember().getEmail();
+
+            /* 여기서 response 이루어짐 */
+            jwtTokenProvider.generateJwtToken(authEmail);
 
             /* JWT 토큰 반환 */
-            return Pair.of(true, jwtTokenProvider.generateJwtToken(authenticatedId, authenticatedEmail));
+            return Pair.of(true, "로그인에 성공했습니다.");
         }
         else{ /* 인증이 되지 않았을 경우 */
-            return Pair.of(false, "로그인에 실패했습니다. 이메일 또는 비밀번호가 일치하는지 확인해주세요.");
+            return Pair.of(false, "로그인에 실패했습니다. 맴버가 존재하는지 확인해주세요.");
         }
     }
 
     /* oauth 서버에서 access_token 받아옴 */
     private String getAccessToken(String authorizationCode, String registration) {
-        //여기 그냥 @Value로 바꿀지 고민 중
         String clientId = env.getProperty("oauth2." + registration + ".client-id");
         String clientSecret = env.getProperty("oauth2." + registration + ".client-secret");
         String redirectUri = env.getProperty("oauth2." + registration + ".redirect-uri");
         String tokenUri = env.getProperty("oauth2." + registration + ".token-uri");
 
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("code", authorizationCode);
-        params.add("client_id", clientId);
-        params.add("client_secret", clientSecret);
-        params.add("redirect_uri", redirectUri);
-        params.add("grant_type", "authorization_code");
+        GoogleTokenDto tokenDto = googleOAuthApi.googleGetToken(authorizationCode, clientId, clientSecret, redirectUri, "authorization_code");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        HttpEntity entity = new HttpEntity(params, headers);
-
-        ResponseEntity<JsonNode> responseNode = restTemplate.exchange(tokenUri, HttpMethod.POST, entity, JsonNode.class);
-        JsonNode accessTokenNode = responseNode.getBody();
-        return accessTokenNode.get("access_token").asText();
+        return tokenDto.getAccessToken();
     }
 
     /* 리다이렉트 URL을 통해서 리소스 가져옴 */
-    private JsonNode getUserResource(String accessToken, String registration) {
+    private GoogleResourceDto getUserResource(String accessToken, String registration) {
         String resourceUri = env.getProperty("oauth2." + registration + ".resource-uri");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + accessToken);
-        HttpEntity entity = new HttpEntity(headers);
-        return restTemplate.exchange(resourceUri, HttpMethod.GET, entity, JsonNode.class).getBody();
+        return googleResourceApi.googleGetResource("Bearer " + accessToken);
     }
 }

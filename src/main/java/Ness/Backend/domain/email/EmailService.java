c4fb55fbd188983bb54463ddb05203994a582e75
JeonHaeseung <414catherine@gmail.com>
2024-03-19 13:19:29 +0900

    [chore] JavaMailSender의 config 완료

---
+++ src/main/java/Ness/Backend/domain/email/EmailService.java
@@ -0,0 +1,16 @@
+package Ness.Backend.domain.email;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+    /*
+    @Async
+    public boolean sendMail()
+     */
+}

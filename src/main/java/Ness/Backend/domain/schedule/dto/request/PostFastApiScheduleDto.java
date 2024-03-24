a5fa1c8ca266dbc094bf9a0f40911fead9255c35
JeonHaeseung <414catherine@gmail.com>
2024-03-24 20:25:02 +0900

    [feat] 백터 DB에 스케쥴 저장하는 로직 완성, 테스트 완료

--- src/main/java/Ness/Backend/domain/schedule/dto/request/PostFastApiScheduleDto.java
+++ src/main/java/Ness/Backend/domain/schedule/dto/request/PostFastApiScheduleDto.java
@@ -1,6 +1,7 @@
 package Ness.Backend.domain.schedule.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,18 +15,29 @@ import java.time.ZonedDateTime;
 @Builder
 public class PostFastApiScheduleDto {
 
+    @JsonProperty("schedule_id")
     private Long schedule_id;
 
+    @JsonProperty("member_id")
     private Long member_id;
 
+    @JsonProperty("data")
     private String info;
 
+    @JsonProperty("location")
     private String location;
 
+    @JsonProperty("person")
     private String person;
 
+    @JsonProperty("schedule_datetime_start")
     @JsonFormat(shape = JsonFormat.Shape.STRING)
-    private ZonedDateTime date;
+    private ZonedDateTime startTime;
 
+    @JsonProperty("schedule_datetime_end")
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private ZonedDateTime endTime;
+
+    @JsonProperty("category")
     private String category;
 }

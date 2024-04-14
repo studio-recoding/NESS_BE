b0280adf6e1922778564b776c5836ae8abdfa391
JeonHaeseung <414catherine@gmail.com>
2024-04-14 17:05:04 +0900

    [feat] docker의 포트포워딩을 80:8080으로 변경

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -11,3 +11,14 @@ then
   sudo systemctl enable docker
   sudo usermod -a -G docker $(whoami)
 fi
+
+if ! type docker-compose > /dev/null
+then
+  echo "docker does not exist"
+  echo "Start installing docker"
+  sudo dnf install -y libxcrypt-compat
+  echo "docker-compose does not exist"
+  echo "Start installing docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi

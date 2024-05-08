7d4d1b0680a5ec00dd6279462ea02a509dd0e105
JeonHaeseung <414catherine@gmail.com>
2024-05-08 20:12:58 +0900

    [feat] 스케쥴 create 및 delete에서 하루 일정 반환하기

--- src/main/java/Ness/Backend/domain/schedule/ScheduleRepository.java
+++ src/main/java/Ness/Backend/domain/schedule/ScheduleRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Repository
@@ -23,7 +24,7 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
             @Param("year") int year,
             @Param("month") int month);
 
-    // 특정 맴버의 한달 치 스케쥴 반환
+    // 특정 맴버의 하루 치 스케쥴 반환(String 사용)
     @Query( value = "SELECT * FROM schedule " +
             "WHERE member_id = :memberId " +
             "AND YEAR(start_time) = :year " +
@@ -31,20 +32,21 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
             "AND DAY(start_time) = :day " +
             "ORDER BY start_time ASC",
             nativeQuery = true)
-    List<Schedule> findOneDaySchedulesByMember_Id(
+    List<Schedule> findOneDaySchedulesByMember_IdWithString(
             @Param("memberId") Long memberId,
             @Param("year") int year,
             @Param("month") int month,
             @Param("day") int day);
 
-    //TODO: 현재는 가장 첫번째 행 데이터를 가져옴->기준을 만들 필요 있음
+    // 특정 맴버의 하루 치 스케쥴 반환(ZoneDateTime 사용)
     @Query( value = "SELECT * FROM schedule " +
             "WHERE member_id = :memberId " +
-            "AND DATE(start_time) = CURDATE() " +
-            "ORDER BY start_time ASC LIMIT 1",
+            "AND DATE(start_time) = DATE(:date) " +
+            "ORDER BY start_time ASC",
             nativeQuery = true)
-    Schedule findTodayOneScheduleByMember_Id(
-            @Param("memberId") Long memberId);
+    List<Schedule> findOneDaySchedulesByMember_Id(
+        @Param("memberId") Long memberId,
+        @Param("date") ZonedDateTime date);
 
     //스케쥴 ID로 특정 스케쥴 찾아주기
     Schedule findScheduleById(Long scheduleId);

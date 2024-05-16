cc913c70e58e4c3d1db2f41dd0cb2835ce28b7ac
JeonHaeseung <414catherine@gmail.com>
2024-05-16 15:13:25 +0900

    [feat] DB에 저장된 페르소나를 적용해 AI에게 전송

--- src/main/java/Ness/Backend/domain/chat/ChatService.java
+++ src/main/java/Ness/Backend/domain/chat/ChatService.java
@@ -9,6 +9,9 @@ import Ness.Backend.domain.chat.entity.Chat;
 import Ness.Backend.domain.chat.entity.ChatType;
 import Ness.Backend.domain.member.MemberRepository;
 import Ness.Backend.domain.member.entity.Member;
+import Ness.Backend.domain.profile.ProfileRepository;
+import Ness.Backend.domain.profile.entity.PersonaType;
+import Ness.Backend.domain.profile.entity.Profile;
 import Ness.Backend.global.fastApi.FastApiChatApi;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +30,7 @@ import java.util.List;
 public class ChatService {
     private final ChatRepository chatRepository;
     private final MemberRepository memberRepository;
+    private final ProfileRepository profileRepository;
     private final FastApiChatApi fastApiChatApi;
 
     /* 새로운 채팅 생성 및 RDB에 저장 */
@@ -43,8 +47,8 @@ public class ChatService {
     }
 
     /* 일주일 치 채팅 데이터 가져오기*/
-    public GetChatListDto getOneWeekUserChat(Long id){
-        List<Chat> chatList = chatRepository.findOneWeekUserChatsByMember_Id(id);
+    public GetChatListDto getOneWeekUserChat(Long memberId){
+        List<Chat> chatList = chatRepository.findOneWeekUserChatsByMember_Id(memberId);
 
         // ChatListResponseDTO에 매핑
         List<GetChatDto> getChatDtos = chatList.stream()
@@ -60,8 +64,9 @@ public class ChatService {
     }
 
     /* 유저의 채팅을 AI에 전송한 후, 답변 및 유저 채팅을 RDB에 저장 */
-    public GetChatListDto postNewUserChat(Long id, PostUserChatDto postUserChatDto){
-        Member memberEntity = memberRepository.findMemberById(id);
+    public GetChatListDto postNewUserChat(Long memberId, PostUserChatDto postUserChatDto){
+        Member memberEntity = memberRepository.findMemberById(memberId);
+        Profile profileEntity = profileRepository.findProfileByMember_Id(memberId);
         //새로운 유저 채팅 저장
         Chat newUserChat = Chat.builder()
                 .createdDate(createdZonedDate())
@@ -73,7 +78,7 @@ public class ChatService {
 
         chatRepository.save(newUserChat);
 
-        PostFastApiAiChatDto AiDto = postNewAiChat(id, postUserChatDto.getText(), postUserChatDto.getChatType());
+        PostFastApiAiChatDto AiDto = postNewAiChat(memberId, postUserChatDto.getText(), postUserChatDto.getChatType(), profileEntity.getPersonaType());
 
         //AI 챗 답변 저장
         Chat newAiChat = Chat.builder()
@@ -86,14 +91,19 @@ public class ChatService {
                 .build();
 
         chatRepository.save(newAiChat);
-        return getOneWeekUserChat(id);
+        return getOneWeekUserChat(memberId);
     }
 
     /* AI에 채팅 전송하는 로직 */
-    public PostFastApiAiChatDto postNewAiChat(Long id, String text, ChatType chatType){
+    public PostFastApiAiChatDto postNewAiChat(Long id, String text, ChatType chatType, PersonaType personaType){
+        String persona = "default";
+        if (personaType == PersonaType.HARDNESS){
+            persona = "hard";
+        }
 
+        //TODO: member_id 전달하는 로직 만들어야 하지 않을까?(RAG를 위해)
         PostFastApiUserChatDto userDto = PostFastApiUserChatDto.builder()
-                .persona("default")
+                .persona(persona)
                 .chatType(chatType) // 유저가 키보드로 친 채팅인지, 아니면 STT를 썼는지 구분
                 .message(text)
                 .build();

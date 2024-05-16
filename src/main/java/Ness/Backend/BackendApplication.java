51ad125abfafb28d3c8dca082ac20ff955e7ea3a
JeonHaeseung <414catherine@gmail.com>
2024-05-16 15:01:08 +0900

    [chore] 사용 안하는 import 문 제거

--- src/main/java/Ness/Backend/BackendApplication.java
+++ src/main/java/Ness/Backend/BackendApplication.java
@@ -2,7 +2,6 @@ package Ness.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class BackendApplication {

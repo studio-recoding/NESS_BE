c68d9b77b205e798b14c0e057860c7c054e54752
JeonHaeseung <414catherine@gmail.com>
2024-05-16 15:00:37 +0900

    [feat] 페르소나를 enum 타입으로 추가

--- src/main/java/Ness/Backend/domain/profile/entity/Profile.java
+++ src/main/java/Ness/Backend/domain/profile/entity/Profile.java
@@ -23,6 +23,10 @@ public class Profile {
 
     private Boolean isEmailActive;
 
+    //유저 페르소나를 구분해주는 타입 값
+    @Enumerated(EnumType.STRING)
+    private PersonaType personaType;
+
     @OneToOne
     @JoinColumn(name = "member_id")
     private Member member;
@@ -35,12 +39,17 @@ public class Profile {
         this.isEmailActive = isEmailActive;
     }
 
+    public void updatePersona(PersonaType personaType){
+        this.personaType = personaType;
+    }
+
     @Builder
-    public Profile(String pictureUrl, String nickname, String name, Member member, Boolean isEmailActive){
+    public Profile(String pictureUrl, String nickname, String name, Member member, Boolean isEmailActive, PersonaType personaType){
         this.pictureUrl = pictureUrl;
         this.nickname = nickname;
         this.name = name;
         this.member = member;
         this.isEmailActive = isEmailActive;
+        this.personaType = personaType;
     }
 }

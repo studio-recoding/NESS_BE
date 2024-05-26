5575ba7a6730173776e508059b9ff3a568084fb7
JeonHaeseung <414catherine@gmail.com>
2024-05-26 16:55:32 +0900

    [fix] REPORT 관련 타임존 모두 변경

--- src/main/java/Ness/Backend/domain/report/ReportActivityRepository.java
+++ src/main/java/Ness/Backend/domain/report/ReportActivityRepository.java
@@ -11,7 +11,7 @@ public interface ReportActivityRepository extends JpaRepository<ReportActivity, 
     // 특정 맴버의 오늘 하루 생성된 데이터만 반환
     @Query( value = "SELECT * FROM report_activity " +
             "WHERE member_id = :memberId " +
-            "AND DATE(created_date) = CURDATE() " +
+            "AND DATE(CONVERT_TZ(created_date, '+00:00', '+09:00')) = CURDATE() " +
             "ORDER BY created_date ASC",
             nativeQuery = true)
     List<ReportActivity> findTodayReportActivityByMember_Id(

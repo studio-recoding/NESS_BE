af025cec6dce4b76d3046692f899486283c24fc2
JeonHaeseung <414catherine@gmail.com>
2024-05-26 17:27:12 +0900

    [chore] import 문 옵티마이제이션

--- src/main/java/Ness/Backend/domain/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/Ness/Backend/domain/auth/jwt/JwtAuthenticationFilter.java
@@ -17,7 +17,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
--- src/main/java/Ness/Backend/domain/schedule/dto/request/PutScheduleDto.java
+++ src/main/java/Ness/Backend/domain/schedule/dto/request/PutScheduleDto.java
@@ -1,7 +1,6 @@
 package Ness.Backend.domain.schedule.dto.request;
 
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;

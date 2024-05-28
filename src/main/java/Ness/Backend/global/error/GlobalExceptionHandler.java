0ab44988869d9953bc3f3835476ab96c5350b36e
JeonHaeseung <414catherine@gmail.com>
2024-05-28 17:49:25 +0900

    [feat] 일반적인 에러 처리 추가

--- src/main/java/Ness/Backend/global/error/GlobalExceptionHandler.java
+++ src/main/java/Ness/Backend/global/error/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
@@ -17,5 +19,13 @@ public class GlobalExceptionHandler {
         log.error("handleBusinessException", baseException);
         final ContentCachingRequestWrapper contentCachingRequestWrapper = new ContentCachingRequestWrapper(httpServletRequest);
         return new ResponseEntity<>(ErrorResponse.onFailure(baseException.getErrorCode(), baseException.getMessage()),null, baseException.getErrorCode().getHttpStatus());
+    }
+
+    // 따로 처리하지 않은 500 에러 모두 처리
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception exception, HttpServletRequest httpServletRequest) {
+        log.error("handleException", exception);
+        final ContentCachingRequestWrapper contentCachingRequestWrapper = new ContentCachingRequestWrapper(httpServletRequest);
+        return new ResponseEntity<>(ErrorResponse.onFailure(ErrorCode._INTERNAL_SERVER_ERROR), null, INTERNAL_SERVER_ERROR);
     }
 }

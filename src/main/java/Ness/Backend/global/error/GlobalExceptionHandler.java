59346624b329878a5b3114573ab8a15cab404119
JeonHaeseung <414catherine@gmail.com>
2024-07-09 19:23:06 +0900

    [fix] 디스코드 알림 중 HttpRequestMethodNotSupportedException은 알림에서 제외

--- src/main/java/Ness/Backend/global/error/GlobalExceptionHandler.java
+++ src/main/java/Ness/Backend/global/error/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +34,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoResourceFoundException.class)
     protected ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException noResourceFoundException, HttpServletRequest httpServletRequest) {
         log.error("handleNoResourceFoundException", noResourceFoundException);
+        final ContentCachingRequestWrapper contentCachingRequestWrapper = new ContentCachingRequestWrapper(httpServletRequest);
+        return new ResponseEntity<>(ErrorResponse.onFailure(ErrorCode._INTERNAL_SERVER_ERROR), null, INTERNAL_SERVER_ERROR);
+    }
+
+    // 잘못된 HTTP 메소드 요청 따로 처리(디스코드 알림에서 제외하기 위함)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException httpRequestMethodNotSupportedException, HttpServletRequest httpServletRequest) {
+        log.error("handleMethodNotSupportedException", httpRequestMethodNotSupportedException);
         final ContentCachingRequestWrapper contentCachingRequestWrapper = new ContentCachingRequestWrapper(httpServletRequest);
         return new ResponseEntity<>(ErrorResponse.onFailure(ErrorCode._INTERNAL_SERVER_ERROR), null, INTERNAL_SERVER_ERROR);
     }

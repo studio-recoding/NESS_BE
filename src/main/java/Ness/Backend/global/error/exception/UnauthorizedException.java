f48726298034c305f99025f6a783d2a0facf37a1
JeonHaeseung <414catherine@gmail.com>
2024-07-09 18:53:20 +0900

    [feat] 리프레시 토큰 관련 에러 처리 추가

---
+++ src/main/java/Ness/Backend/global/error/exception/UnauthorizedException.java
@@ -0,0 +1,17 @@
+package Ness.Backend.global.error.exception;
+
+import Ness.Backend.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class UnauthorizedException extends BaseException {
+    public UnauthorizedException() {
+        super(ErrorCode.UNAUTHORIZED_ACCESS, ErrorCode.UNAUTHORIZED_ACCESS.getMessage());
+    }
+    public UnauthorizedException(String message) {
+        super(ErrorCode.UNAUTHORIZED_ACCESS, message);
+    }
+    public UnauthorizedException(ErrorCode errorCode) {
+        super(errorCode, errorCode.getMessage());
+    }
+}

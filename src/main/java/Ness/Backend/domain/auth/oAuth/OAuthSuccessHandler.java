f8d8831e89af9fbf68761a017088ec1321edffc4
JeonHaeseung <414catherine@gmail.com>
2024-07-09 18:52:28 +0900

    [feat] OAUTH 핸들러 종속성 관리

--- src/main/java/Ness/Backend/domain/auth/oAuth/OAuthSuccessHandler.java
+++ src/main/java/Ness/Backend/domain/auth/oAuth/OAuthSuccessHandler.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
